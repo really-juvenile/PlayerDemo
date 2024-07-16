fe8dd1d5be829c256c12f6d04d586ec6756bdeb0
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-07-16 12:32:31 +0530

    Add project files.

---
+++ Models/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerDemo.Models
+{
+    internal class Player
+    {
+        const int DEFAULT_AGE = 18;
+        public int id { get; }
+        public string name { get; }
+        public int age { get; } = DEFAULT_AGE;
+
+        
+
+
+        public Player(int Id,string Name)
+        {
+            id = Id;
+            name = Name;
+
+        }
+
+        public Player(int Id, string Name,int Age) : this(Id,Name)
+        {
+            age =Age;
+        }
+
+        public static Player WhoIsElder(Player[] players)
+        {
+            Player playerInUse = null;
+            int max_age = DEFAULT_AGE;
+            foreach(Player player1 in players)
+            {
+                 if(player1.age > max_age )
+                {
+                    playerInUse=player1;
+                }               
+
+            }
+            return playerInUse;
+        }
+
+       
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using PlayerDemo.Models;
+namespace PlayerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1;
+            Player player2;
+            Player player3;
+
+            Player[] players = {
+                player1= new Player(101, "Allen", 21), 
+                player2= new Player(102, "Mary", 33),
+                player3= new Player(103, "Arjun", 36)
+            };
+            Player oldest = Player.WhoIsElder(players);
+            Console.WriteLine($"The person {oldest.name} with highest age {oldest.age} ");
+
+        }
+    }
+}
